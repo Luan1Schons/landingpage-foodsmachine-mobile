6d2953f23608a4938859207122d232fe4110b627
Luan Schons <luanschons2000@gmail.com>
2021-09-03 13:27:13 -0300

    up

---
+++ assets/js/app.js
@@ -0,0 +1,65 @@
+$('.carousel-perfect-for-all').owlCarousel({
+  loop: true,
+  center: true,
+  margin: 10,
+  nav: false,
+  items: 1,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  singleItem: true,
+  slideSpeed: 300,
+  paginationSpeed: 400,
+})
+
+
+$('.clients-carousel').owlCarousel({
+  loop: true,
+  center: true,
+  margin: 10,
+  nav: false,
+  items: 2,
+  autoplay: true,
+  autoplayTimeout: 3000,
+  singleItem: true,
+  slideSpeed: 300,
+  paginationSpeed: 400,
+})
+
+$(document).ready(function () {
+
+  let clickFunctionalities = 0;
+  $('.btn-red-functionalities').on('click', function (e) {
+    if (clickFunctionalities === 0) {
+      clickFunctionalities = 1;
+      $('.brands').addClass('hideBrands');
+    } else {
+      clickFunctionalities = 0;
+      $('.brands').removeClass('hideBrands');
+    }
+  })
+
+
+  $('body').on('click','#btnNeedHelpRegister', function(e) {
+    e.preventDefault();
+    $('#btnAddModuleRegister').removeClass('btn-cian').addClass('btn-disabled');
+    $('#regiser-showcase').hide();
+    $(this).removeClass( "btn-white" ).addClass( "btn-dark" );
+    $(this).text('OK, ENTRAREMOS EM CONTATO O MAIS RÁPIDO POSSÍVEL')
+  });
+
+  $('body').on('click','#btnAddModuleRegister', function(e) {
+    e.preventDefault();
+    $('#btnAddModuleRegister').removeClass('btn-cian').removeClass('btn-disabled').addClass('btn-dark');
+    $('#regiser-showcase').show();
+    $('#btnNeedHelpRegister').removeClass( "btn-dark" ).addClass( "btn-white" );
+    $('.need-help-to-mount-plan').html('<button type="button" class="btn btn-white" id="btnNeedHelpRegister"><b>PRECISA</b> DE AJUDA PARA MONTAR SEU PLANO</button>');
+  });
+
+  $('.register-btn').on('click', function(e) {
+    e.preventDefault();
+    //$('#btnAddModuleRegister').removeClass('btn-disabled').addClass('btn-cian');
+    //$('#regiser-showcase').show();
+  });
+
+});
+
